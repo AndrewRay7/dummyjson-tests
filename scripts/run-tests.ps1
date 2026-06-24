4d49486469404ab470877245d9fdba4fd1fb0ace
AndrewRay <19Andrewray@gmail.com>
2026-06-25 00:16:35 +0300

    Fix Readme.md

--- scripts/run-tests.ps1
+++ scripts/run-tests.ps1
@@ -1,7 +1,3 @@
-# Устанавливаем кодировку вывода в UTF-8
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$OutputEncoding = [System.Text.Encoding]::UTF8
-
 Write-Host "ZAPUSK TESTOV DummyJSON API..." -ForegroundColor Cyan
 
 New-Item -ItemType Directory -Force -Path ".\reports" | Out-Null
